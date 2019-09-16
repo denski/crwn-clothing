9acb72a79b11b7c6142f656354d61d08d2d07d98
Dennis <denski23@hotmail.com>
2019-09-16 13:19:51 +0800

    add firestore into the project

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
+import { async } from 'q';
 
 const config = {
   apiKey: 'AIzaSyAmhy7eu4X9mwfBKMdWCEhCfHCccSCYbg4',
@@ -10,6 +11,32 @@ const config = {
   storageBucket: '',
   messagingSenderId: '719328436670',
   appId: '1:719328436670:web:485896654ea6ed9bb619ef',
+};
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
 };
 
 firebase.initializeApp(config);
