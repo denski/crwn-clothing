622adbaa82b390132c1b65915359666a4193c994
Dennis <denski23@hotmail.com>
2019-09-16 14:37:36 +0800

    adding the sign-up component

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
-import { async } from 'q';
 
 class App extends React.Component {
   constructor() {
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,7 +1,6 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
-import { async } from 'q';
 
 const config = {
   apiKey: 'AIzaSyAmhy7eu4X9mwfBKMdWCEhCfHCccSCYbg4',
